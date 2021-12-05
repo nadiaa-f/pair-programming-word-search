34008dd34d6b1ec7ca96c1f6fd361e3ccb738622
Nadia Fatah <nadi.fatah@gmail.com>
2021-12-05 09:38:46 +0000

    function wordSearchpassed all tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,53 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+
+const wordSearch = (letters, word) => {
+    let result = false;
+  
+    if (letters.length < 1 || word.length < 1) {
+      result = false;
     }
-}
+  
+  
+    const horizontalJoin = letters.map(ls => ls.join(''));
+  
+    let vertical = [];
+  
+    for (let row = 0; row < letters.length; row++) {
+      for (let column = 0; column < letters[row].length; column++) {
+        if (row === 0) {
+          vertical.push([letters[row][column]]);
+        } else {
+          vertical[column].push(letters[row][column]);
+        }
+      }
+  
+    }
+  
+    const verticalJoin = vertical.map(ls => ls.join(''));
+    console.log(verticalJoin);
+     
+      
+    for (let l of verticalJoin) {
+      console.log(l);
+        
+      if (l.includes(word)) {
+        console.log("Found");
+        result = true;
+        break;
+      }
+          
+    }
+  
+    for (let l of horizontalJoin) {
+      console.log(l);
+        
+      if (l.includes(word)) {
+        console.log("Found");
+        result = true;
+        break;
+      }
+          
+    }
+    return result;
+  };
 
 module.exports = wordSearch
